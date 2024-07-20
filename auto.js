7f158b0edbc10d8b489066cb7832911fa165b344
Neha Bharti <141570516+nehabharti123@users.noreply.github.com>
2024-07-20 20:19:50 +0530

    Add files via upload

---
+++ auto.js
@@ -0,0 +1,32 @@
+const availableKeywords = [
+    'Google',
+    'YouTube',
+    'Instagram',
+    'GSOC',
+    'kiwix',
+    'Twitter',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function() {
+    let result =[];
+    let input = inputBox.value;
+    if(input.length){
+     result = availableKeywords.filter((keyword) =>{
+        return keyword.toLowerCase().includes(input.toLowerCase());
+  
+});
+console.log(result);
+    }
+    display(result);
+}
+
+
+function display(result) {
+    const content = result.map((list) => {
+    return "<li>" + list + "</li>";
+    });
+    resultsBox.innerHTML = "<ul>" + content + "</li>";
+}
